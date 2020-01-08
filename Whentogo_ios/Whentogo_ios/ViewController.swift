eeabc3c3dbaa5935a5712072c5bd748775ec1083
정윤도 <truenorthj@gmail.com>
2020-01-09 05:37:32 +0900

    updated

--- Whentogo_ios/Whentogo_ios/ViewController.swift
+++ Whentogo_ios/Whentogo_ios/ViewController.swift
@@ -13,7 +13,31 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
   
+    @IBOutlet weak var destination: UISearchBar!
+    @IBOutlet weak var arrivaltime: UIDatePicker!
+    @IBOutlet weak var searchtime: UIButton!
+    @IBOutlet weak var departtext: UILabel!
+    @IBOutlet weak var departtime: UILabel!
+    @IBOutlet weak var timeleftshow: UILabel!
     @IBOutlet weak var jsontext: UILabel!
+    
+   
+    @IBAction func searchtimebtn(_ sender: Any) {
+        
+    }//가는곳을 받아서 값계산하기
+    
+    @IBAction func startbtn(_ sender: Any) {
+        
+    }//출발까지카운트다운시작하기
+    
+    @IBAction func telldeparture(_ sender: Any) {
+        
+        
+    }//현재gps가 목표gps와 같을경우 나타내기
+    
+    func timecal()->(){
+        
+    }//출발까지카운트다운
     
     override func viewDidLoad() {
         ODsayService.sharedInst().setApiKey("qPt1EFCkMnXiERhw5sw4ag")
@@ -39,11 +63,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
            let lat = placemark?.location?.coordinate.latitude
            let lon = placemark?.location?.coordinate.longitude
         }
-    }
+    }//목표장소 gps구하기
     
 
-     let locationManager = CLLocationManager()
+    let locationManager = CLLocationManager()
 
+    
     override func viewDidAppear(_ animated: Bool) {
         self.locationManager.delegate = self                  // 뷰 컨트롤러를 로케이션 매니저 객체에 대한 앱델리게이트로 선언
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest  // 정확도 : 최고
